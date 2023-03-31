4a5f455dea7e4db83575b268af93f145a6da5fcf
Demigod916 <60995067+Demigod916@users.noreply.github.com>
2023-03-31 11:04:58 -0700

    Update server.lua

--- lab-Combine/server.lua
+++ lab-Combine/server.lua
@@ -7,13 +7,23 @@ local Combinations = {
 }
 
 local combhook = ox_inventory:registerHook('swapItems', function(payload)
-    if  payload.fromSlot ~= nil and payload.toSlot ~= nil and Combinations[payload.fromSlot.name] ~= nil and payload.toSlot.name == Combinations[payload.fromSlot.name].needs then
+    if payload.fromSlot ~= nil and payload.toSlot ~= nil then
+        local item1 = (Combinations[payload.fromSlot.name] and payload.fromSlot.name) or (Combinations[payload.toSlot.name] and payload.toSlot.name)
+
+        if not item1 then return end
+
+        local item2 = (Combinations[item1].needs == payload.fromSlot.name and payload.fromSlot.name) or (Combinations[item1].needs == payload.toSlot.name and payload.toSlot.name)
+        if not item2 then return end
+
+        if Combinations[payload.fromSlot.name] then
+            item1 = payload.fromSlot.name
+        end
         TriggerClientEvent('ox_inventory:closeInventory', payload.source)
         TriggerClientEvent('lab-Combine:Combine', payload.source)
         Wait(2000)
-        ox_inventory:RemoveItem(payload.source, payload.fromSlot.name, 1)
-        ox_inventory:RemoveItem(payload.source, Combinations[payload.fromSlot.name].needs, 1)
-        ox_inventory:AddItem(payload.source, Combinations[payload.fromSlot.name].result, 1)
+        ox_inventory:RemoveItem(payload.source, item1, 1)
+        ox_inventory:RemoveItem(payload.source, item2, 1)
+        ox_inventory:AddItem(payload.source, Combinations[item1].result, 1)
         return false
     end
-end,{})
+end, {})
