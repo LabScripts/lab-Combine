8592d6cde869e89d8ab8f0b8d4a5adfd773c1902
Demigod916 <60995067+Demigod916@users.noreply.github.com>
2023-04-02 03:31:30 -0700

    added amount for output item

--- lab-Combine/server.lua
+++ lab-Combine/server.lua
@@ -17,9 +17,17 @@ local Combinations = { --create new item craft Recipies here. limited to 2 items
         result = 'packagedmeth' -- the item you get after craft
     },
 
+
+    ['garbage'] = { --item1
+        otherItem = 'scrapmetal', --item2
+        duration = 2000, --how long the craft takes
+        result = 'lockpick', -- the item you get after craft
+        amount = {['garbage'] = 1, ['scrapmetal'] = 2, ['lockpick'] = 2}, --amount of both items needed to craft and then how many of the crafted item it makes. must match item1, item2, result
+    },
+
 }
 
----@type table<number, {item1: string, item2: string, result: string}>
+---@type table<number, table<string, {name: string, amount: number}>>
 local CraftQueue = {}
 
 local craftHook = ox_inventory:registerHook('swapItems', function(data)
@@ -30,7 +38,8 @@ local craftHook = ox_inventory:registerHook('swapItems', function(data)
         local item1 = (Combinations[data.fromSlot?.name] and data?.fromSlot) or (Combinations[data.toSlot?.name] and data.toSlot)
         if not item1 then return end
 
-        if Combinations[item1.name].amount[item1.name] > item1.count then
+        local amount1 = Combinations[item1.name].amount[item1.name]
+        if amount1 > ox_inventory:GetItem(data.source, item1.name, nil, true)  then
             TriggerClientEvent('ox_lib:notify', data.source,
             {
                 type = 'error',
@@ -42,7 +51,8 @@ local craftHook = ox_inventory:registerHook('swapItems', function(data)
         local item2 = (Combinations[item1.name].otherItem == data.fromSlot.name and data.fromSlot) or (Combinations[item1.name].otherItem == data.toSlot.name and data.toSlot)
         if not item2 then return end
 
-        if Combinations[item1.name].amount[item2.name] > item2.count then
+        local amount2 = Combinations[item1.name].amount[item2.name]
+        if amount2 > ox_inventory:GetItem(data.source, item2.name, nil, true) then
             TriggerClientEvent('ox_lib:notify', data.source,
             {
                 type = 'error',
@@ -50,10 +60,16 @@ local craftHook = ox_inventory:registerHook('swapItems', function(data)
             })
             return false
         end
-        print("craft")
+
         TriggerClientEvent('lab-Combine:Combine', data.source, Combinations[item1.name].duration)
 
-        CraftQueue[data.source] = {item1 = item1.name, item2 = item2.name, result = Combinations[item1.name].result}
+        local resultItem = Combinations[item1.name].result
+
+        CraftQueue[data.source] = {
+            item1 = {name = item1.name, amount = amount1},
+            item2 = {name = item2.name, amount = amount2},
+            result = {name = resultItem, amount = Combinations[item1.name].amount[resultItem]}
+        }
 
         return false
     end
@@ -66,9 +82,9 @@ lib.callback.register('lab-Combine:success', function(source, success)
     if not data then return end
 
     if success then
-        ox_inventory:RemoveItem(source, data.item1, 1)
-        ox_inventory:RemoveItem(source, data.item2, 1)
-        ox_inventory:AddItem(source, data.result, 1)
+        ox_inventory:RemoveItem(source, data.item1.name, data.item1.amount)
+        ox_inventory:RemoveItem(source, data.item2.name, data.item2.amount)
+        ox_inventory:AddItem(source, data.result.name, data.result.amount)
     end
 
     CraftQueue[source] = nil
