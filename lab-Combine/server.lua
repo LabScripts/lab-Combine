ff41114965ba22143eb3474b66d312bccca66623
Dogo-Lab <97763126+LabScripts@users.noreply.github.com>
2023-04-02 14:22:32 +0300

    Update server.lua

--- lab-Combine/server.lua
+++ lab-Combine/server.lua
@@ -1,19 +1,41 @@
 local ox_inventory = exports.ox_inventory
 
 local Combinations = {
- -- ['item you drag'] = {needs = 'item you drag on to', result = 'result item'},
-	['coca'] = {needs = 'baggy', result = 'coke'},
-    ['meth'] = {needs = 'baggy', result = 'packagedmeth'},
+    -- ['something'] = {
+    --     needs = 'the other something', 
+    --     result = {
+    --         [1] = {name = 'some result', amount = 1},
+    --         --[2] = {name = 'more results', amount = 5},
+    --     }, 
+    --     removeItemA = true, -- Remove the dragged item?
+    --     removeItemB = true, -- Remove the item you dragged onto?
+    -- },
+
+	['coca'] = {
+        needs = 'baggy', 
+        result = {
+            [1] = {name = 'coke', amount = 1},
+            --[2] = {name = 'something', amount = 5},
+        }, 
+        removeItemA = true, -- Remove the dragged item?
+        removeItemB = true, -- Remove the item you dragged onto?
+    },
 }
 
 local combhook = ox_inventory:registerHook('swapItems', function(payload)
-    if  payload.fromSlot ~= nil and payload.toSlot ~= nil and Combinations[payload.fromSlot.name] ~= nil and payload.toSlot.name == Combinations[payload.fromSlot.name].needs then
+    if  payload.fromInventory == payload.source and payload.fromSlot ~= nil and payload.toSlot ~= nil and Combinations[payload.fromSlot.name] ~= nil and payload.toSlot.name == Combinations[payload.fromSlot.name].needs then
         TriggerClientEvent('ox_inventory:closeInventory', payload.source)
         TriggerClientEvent('lab-Combine:Combine', payload.source)
         Wait(2000)
-        ox_inventory:RemoveItem(payload.source, payload.fromSlot.name, 1)
-        ox_inventory:RemoveItem(payload.source, Combinations[payload.fromSlot.name].needs, 1)
-        ox_inventory:AddItem(payload.source, Combinations[payload.fromSlot.name].result, 1)
+        if Combinations[payload.fromSlot.name].removeItemA then
+            ox_inventory:RemoveItem(payload.source, payload.fromSlot.name, 1)
+        end
+        if Combinations[payload.fromSlot.name].removeItemB then
+            ox_inventory:RemoveItem(payload.source, Combinations[payload.fromSlot.name].needs, 1)
+        end
+        for k,v in pairs(Combinations[payload.fromSlot.name].result) do
+            ox_inventory:AddItem(payload.source, v.name, v.amount)
+        end
         return false
     end
 end,{})
